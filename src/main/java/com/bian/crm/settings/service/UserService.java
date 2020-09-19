78895fdad917e3e86aeceb4512fa43c16a0ff959
NicholasBian <n381636597@gmail.com>
2020-09-19 23:50:55 +0800

    Learning how to build a CRM project

--- src/main/java/com/bian/crm/settings/service/UserService.java
+++ src/main/java/com/bian/crm/settings/service/UserService.java
@@ -1,4 +1,7 @@
 package com.bian.crm.settings.service;
 
+import com.bian.crm.settings.domain.User;
+
 public interface UserService {
+    User login(String loginAct, String loginPwd, String ip);
 }
--- src/main/java/com/bian/crm/settings/web/controller/UserController.java
+++ src/main/java/com/bian/crm/settings/web/controller/UserController.java
@@ -1,22 +1,77 @@
 package com.bian.crm.settings.web.controller;
 
+import com.bian.crm.settings.domain.User;
+import com.bian.crm.settings.service.UserService;
+import com.bian.crm.settings.service.impl.UserServiceImpl;
+import com.bian.crm.utils.MD5Util;
+import com.bian.crm.utils.PrintJson;
+import com.bian.crm.utils.ServiceFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserController extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException{
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("进入到用户控制器");
 
         String path = request.getServletPath();
 
-        if("/settings/user/xxx.do".equals(path)){
-            //xxx(request, response);
+        if("/settings/user/login.do".equals(path)){
+            login(request, response);
         }else if("/settings/user/xxx.do".equals(path)){
             //xxx(request, response);
         }
+    }
+
+    private void login(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("进入到验证登陆的操作");
+        String loginAct = request.getParameter("loginAct");
+        String loginPwd = request.getParameter("loginPwd");
+        //将密码的明文形式转换为MD5的密文形式
+        loginPwd = MD5Util.getMD5(loginPwd);
+        //接收ip地址
+        String ip = request.getRemoteAddr();
+        System.out.println("----------ip-----------:"+ip);
+        //未来业务层开发，统一使用代理类形态的接口对象
+        UserService us = (UserService) ServiceFactory.getService(new UserServiceImpl());
+
+        try{
+            User user = us.login(loginAct, loginPwd, ip);
+            request.getSession().setAttribute("user", user);
+            //如果程序执行到此处，说明业务层没有为controller抛出任何异常，登陆成功
+            /*
+            {"success":true}
+             */
+            PrintJson.printJsonFlag(response,true);
+        }catch (Exception e){
+            e.printStackTrace();
+            /*
+            {"success":true, "msg":?}
+             */
+            String msg = e.getMessage();
+            /*
+            我们现在作为controller，需要为ajax请求提供多项信息
+            可以有两种手段来处理：
+            1）将多项信息打包成map，将map解析为json串
+            2）创建一个vo
+                private boolean success;
+                private String msg;
+            如果对于展现的信息将来还会大量使用，我们创建vo类，使用方便；
+            如果对于展现的信息只有在这个需求中能够使用，我们使用map就可以。
+             */
+            Map<String,Object> map = new HashMap<String,Object>();
+            map.put("success",false);
+            map.put("msg",msg);
+            PrintJson.printJsonObj(response,map);
+        }
+
+
     }
 }
